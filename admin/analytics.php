52e8dd788efb1ee2c09a01a866cbcb3e37ea59c9
agricart007 <agricart007@gmail.com>
2024-01-08 19:19:01 +0530

    navbar created

--- admin/analytics.php
+++ admin/analytics.php
@@ -8,95 +8,9 @@
     <link rel="stylesheet" href="admin.css">
 </head>
 <body>
-    <input type="checkbox" name="" id="menu-toggle">
-    <div class="sidebar">
-        <div class="sidebar-container">
-            <div class="brand">
-                <h2>
-                    <span class="las la-tools"></span>
-                    Agricart
-                </h2>
-            </div>
-
-            <div class="sidebar-avartar">
-                <div>
-                    <img src="images/adminfarmerlogo.png" alt="">
-                </div>
-                <div class="avartar-info">
-                    <div class="avartar-text">
-                        <h4>
-                            Tom Cruise
-                        </h4>
-                        <small>
-                            1092-1234-235
-                        </small>
-
-                    </div>
-                    <span class="las la-angle-double-down"></span>
-                </div>
-            </div>
-
-            <div class="sidebar-menu">
-                <ul>
-                    <li>
-                        <a href="">
-                            <span class="las la-adjust"></span>
-                            <span>Dasboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="las la-chart-bar"></span>
-                            <span> Sales</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="" class="active">
-                            <span class="las la-chart-pie"></span>
-                            <span>Analytics</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="las la-user"></span>
-                            <span>Buyer</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="las la-user"></span>
-                            <span>Seller</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="las la-cog"></span>
-                            <span>Settings</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-
-            <div class="sidebar-card">
-                <div class="side-card-icon">
-                    <span class="lab la-codiepie">
-
-                    </span>
-                </div>
-                <div>
-                    <h4>
-                        Make Ad
-                    </h4>
-                    <p>
-                        Add ads to your videos to earn money
-                    </p> 
-                </div>
-                <button class="btn btn-main btn-block">Create Now</button>
-            </div>
-        </div>
-    </div>
-
+   
+    <?php include ("navbar.php");
+    ?>
     <div class="main-content">
         <header>
             <div class="header-title-wrapper">
