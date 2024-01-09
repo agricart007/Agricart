d9633a4d031ee3b48a279e12042b643068eb72f4
agricart007 <agricart007@gmail.com>
2024-01-09 11:12:14 +0530

    footer remove

---
+++ buyer/footer.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="home.css">
+    </head>
+    <body>
+    <footer class="section-p1">
+            <div class="col">
+                <img src="../images/homelogo.png" class="logo">
+                <h4>contact</h4>
+                <p><strong>Address:</strong>------------------</p>
+                <p><strong>Phone:</strong>------------------</p>
+                <p><strong>Hours:</strong>------------------</p>
+                <div class="follow">
+                    <h4>Follow us</h4>
+                    <div class="icon">
+                        <ion-icon name="logo-facebook"></ion-icon>
+                        <ion-icon name="logo-instagram"></ion-icon>
+                        <ion-icon name="logo-youtube"></ion-icon>
+                    </div>
+                </div>                
+            </div>
+
+            <div class="col">
+                <h4>About</h4>
+                <a href="#">About us</a>
+                <a href="#">contact us</a>
+                <a href="#">Terma & Condition</a>
+            </div>
+
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign In</a>
+                <a href="#">Log in</a>
+                <a href="#">Help</a>
+            </div>
+
+            <div class="col install">
+                <h4>Install App</h4>
+                <p>Form App Store or Google Play</p>
+                <div class="row">
+                    <img src="../images/google-apple-badge.jpg" class="logo-ga">
+                </div>
+                <p>Secure Payment Gateway</p>
+                <img src="../images/payment-badge.png" class="logo-pay">
+            </div>
+        </footer>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </body>
+    </html>
